d0de19d01307a3e69491eb4b668158192d9ab5de
Niko <nikosssmirnov@gmail.com>
2022-12-14 20:35:24 +0800

    binary

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,3 +1,19 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
+    let lo = 0;
+    let hi = haystack.length;
 
+    do {
+        const m = Math.floor(lo + (lo - hi) / 2);
+        const v = haystack[m];
+
+        if (v === needle) {
+            return true;
+        } else if (v > needle){
+            hi = m;
+        } else {
+            lo = m;
+        }
+    } while (lo < hi);
+
+    return false;
 }
